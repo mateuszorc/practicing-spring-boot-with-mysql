14ecafae89f3c3b657f73eefee9a5897735d7670
mateuszorc <57670452+mateuszorc@users.noreply.github.com>
2022-02-07 13:15:07 +0100

    added CRUD methods to the project
    modified: StudentController,StudentService, StudentRepository (#1)

--- src/main/java/com/practice/practicingspringboot/controllers/StudentController.java
+++ src/main/java/com/practice/practicingspringboot/controllers/StudentController.java
@@ -3,8 +3,7 @@ package com.practice.practicingspringboot.controllers;
 import com.practice.practicingspringboot.model.Student;
 import com.practice.practicingspringboot.services.StudentService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 import java.time.LocalDate;
 import java.time.Month;
@@ -24,5 +23,20 @@ public class StudentController {
     @GetMapping
     public List<Student> getStudents() {
         return studentService.getStudents();
+    }
+
+    @PostMapping
+    public  void addStudent(@RequestBody Student student) {
+        studentService.addNewStudent(student);
+    }
+
+    @DeleteMapping(path = "{studentId}")
+    public void deleteStudent(@PathVariable("studentId") Long studentId) {
+        studentService.deleteStudent(studentId);
+    }
+
+    @PutMapping()
+    public Student updateStudent (@RequestBody Student student) {
+        return studentService.updateStudent(student);
     }
 }
--- src/main/java/com/practice/practicingspringboot/services/StudentService.java
+++ src/main/java/com/practice/practicingspringboot/services/StudentService.java
@@ -5,9 +5,12 @@ import com.practice.practicingspringboot.repositories.StudentRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import javax.transaction.Transactional;
 import java.time.LocalDate;
 import java.time.Month;
 import java.util.List;
+import java.util.Objects;
+import java.util.Optional;
 
 @Service
 public class StudentService {
@@ -21,5 +24,49 @@ public class StudentService {
 
     public List<Student> getStudents() {
         return studentRepository.findAll();
+    }
+
+    public void addNewStudent(Student student) {
+        Optional<Student> studentOptional = studentRepository.findStudentByEmail(student.getEmail());
+
+        if(studentOptional.isPresent()) {
+            throw new IllegalStateException("email taken");
+        }
+
+        studentRepository.save(student);
+    }
+
+    public void deleteStudent(Long studentId) {
+        boolean exists = studentRepository.existsById(studentId);
+        if(!exists) {
+            throw new IllegalStateException("student with id " + studentId + " does not exist");
+        }
+        studentRepository.deleteById(studentId );
+    }
+
+    @Transactional
+    public Student updateStudent(Student student) {
+
+        Student studentEdited = studentRepository.findById(student.getId())
+                .orElseThrow(() -> new IllegalStateException("student with id " + student + " does not exist"));
+
+        String name = student.getName();
+        String email = student.getEmail();
+
+        if (name != null && name.length() > 0 && !Objects.equals(studentEdited.getName(), name)) {
+            studentEdited.setName(student.getName());
+        }
+
+        if(email != null && email.length() > 0 && !Objects.equals(studentEdited.getEmail(), email)) {
+            Optional<Student> studentOptional = studentRepository.findStudentByEmail(email);
+            if (studentOptional.isPresent()) {
+                 throw new IllegalStateException("email taken");
+            }
+            studentEdited.setEmail(student.getEmail());
+        }
+
+
+        return studentEdited;
+
     }
 }
