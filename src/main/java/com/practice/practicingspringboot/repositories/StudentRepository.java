6124e1031cdb1e1a046919c13f1c32c80b2763ce
czesiop <m.orczewski@gmail.com>
2022-02-07 13:09:25 +0100

    added CRUD methods to the project, modified: StudentController,StudentService, StudentRepository

--- src/main/java/com/practice/practicingspringboot/repositories/StudentRepository.java
+++ src/main/java/com/practice/practicingspringboot/repositories/StudentRepository.java
@@ -2,8 +2,14 @@ package com.practice.practicingspringboot.repositories;
 
 import com.practice.practicingspringboot.model.Student;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
 
 @Repository
 public interface StudentRepository extends JpaRepository<Student, Long> {
+
+    @Query("SELECT s FROM Student s WHERE s.email = ?1")
+    Optional<Student> findStudentByEmail(String email);
 }
